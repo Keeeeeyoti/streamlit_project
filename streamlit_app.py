886b25b5df4fb6181bf9c6189616d9266f9d44d2
Keeeeeyoti <129565494+Keeeeeyoti@users.noreply.github.com>
2024-10-16 22:02:47 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,16 +9,12 @@ st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?l
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('dp.png'))
 
-st.header('Chanin Nantasenamat, Ph.D.')
+st.header('Keeeeeyoti, Ph.D.')
 
-st.info('Developer Advocate, Content Creator and ex-Professor with an interest in Data Science and Bioinformatics')
+st.info('professional coin flipper')
 
 icon_size = 20
 
-st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+st_button('youtube', 'https://www.youtube.com/@keeeeeyoti', 'Keyoti YouTube channel', icon_size)
+st_button('twitter', 'https://twitter.com/TYKKYTYTK', 'Follow me on Twitter', icon_size)
+st_button('Soundcloud', 'https://soundcloud.com/keyoti', 'Follow me on Soundcloud', icon_size)
